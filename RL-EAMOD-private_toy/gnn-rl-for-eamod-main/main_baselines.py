551108cc27cd785d2fe53c604758dea2555edc8e
AaryanSinghal <aaryan0404@gmail.com>
2023-03-08 01:51:03 -0800

    h

--- RL-EAMOD-private_toy/gnn-rl-for-eamod-main/main_baselines.py
+++ RL-EAMOD-private_toy/gnn-rl-for-eamod-main/main_baselines.py
@@ -50,10 +50,12 @@ parser.add_argument('--directory', type=str, default='saved_files',
                     help='defines directory where to save files')
 
 args = parser.parse_args()
-args.cuda = torch.cuda.is_available()
-device = torch.device("cuda" if args.cuda else "cpu")
+# args.cuda = torch.cuda.is_available()
+# device = torch.device("cuda" if args.cuda else "cpu")
+device = torch.device("cpu")
 
-problem_folder = 'Toy'
+# problem_folder = 'Toy'
+problem_folder = 'TrainingData'
 file_path = os.path.join('data', problem_folder, 'scenario_train_3_2.json')
 experiment = args.baseline_type + '_baseline_testing_' + file_path
 energy_dist_path = os.path.join('data', problem_folder, 'energy_distance_3x2.npy')
