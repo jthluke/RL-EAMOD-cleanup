cb875dbbbcd2fb0e56eed9bf972837acf6f13379
AaryanSinghal <aaryan0404@gmail.com>
2023-03-08 02:22:26 -0800

    h

--- RL-EAMOD-private_toy/gnn-rl-for-eamod-main-heuristic/main.py
+++ RL-EAMOD-private_toy/gnn-rl-for-eamod-main-heuristic/main.py
@@ -94,7 +94,7 @@ T = args.T
 if args.toy:
     problem_folder = 'Toy'
     # file_path = os.path.join('data', problem_folder, 'scenario_test_6_1x2_flip.json')
-    file_path = os.path.join('data', 'TrainingData', 'scenario_test_6_1x2_flip.json')
+    file_path = os.path.join('data', 'scenario_test_6_1x2_flip.json')
     experiment = 'training_' + problem_folder+ '_' + str(args.max_episodes) + '_episodes_T_' + str(args.T) + file_path + '_heuristic_' + charging_heuristic + "_fast_charging"
     energy_dist_path = os.path.join('data', problem_folder,  'energy_distance_1x2.npy')
     scenario = create_scenario(file_path, energy_dist_path)
