9214034e423764433c552312602986f9dcd5f716
AaryanSinghal <aaryan0404@gmail.com>
2023-03-08 01:57:20 -0800

    h

--- RL-EAMOD-private_toy/gnn-rl-for-eamod-main/main_baselines.py
+++ RL-EAMOD-private_toy/gnn-rl-for-eamod-main/main_baselines.py
@@ -12,7 +12,7 @@ import wandb
 from src.envs.amod_env import Scenario, AMoD
 from src.algos.a2c_gnn import A2C
 # from src.algos.reb_flow_solver import solveRebFlow
-from src.algos.reb_flows_solver import solveRebFlow
+from src.algos.reb_flows_solver import RebalFlowSolver
 from src.misc.utils import dictsum
 
 def create_scenario(json_file_path, energy_file_path, seed=10):
@@ -119,7 +119,10 @@ while(not done):
     for n in env.nodes:
         assert desiredAcc[n] >= 0
     # solve minimum rebalancing distance problem (Step 3 in paper)
-    rebAction = solveRebFlow(env=env, desiredAcc=desiredAcc, gurobi_env=gurobi_env)
+
+    # rebAction = solveRebFlow(env=env, desiredAcc=desiredAcc, gurobi_env=gurobi_env)
+    rebAction = RebalFlowSolver(env=env, desiredAcc=desiredAcc, gurobi_env=gurobi_env).solve()
+    
     # Take action in environment
     new_obs, rebreward, done, info = env.reb_step(rebAction)
     episode_reward += rebreward
