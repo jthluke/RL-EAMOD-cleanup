090df4ac96947dfb7a48b73acc5fb0384c323c11
AaryanSinghal <aaryan0404@gmail.com>
2023-03-08 23:20:36 -0800

    h

--- RL-EAMOD-private_toy/gnn-rl-for-eamod-main/mpc_baselines/mpc_ipynb.py
+++ RL-EAMOD-private_toy/gnn-rl-for-eamod-main/mpc_baselines/mpc_ipynb.py
@@ -11,22 +11,6 @@ import numpy as np
 import gurobipy as gp
 import json
 
-# MPC exact
-number_nodes = 5
-number_charge_levels = 5
-chargers = []
-for node in range(number_nodes):
-    if node%2 ==1:
-        chargers.append(node)
-f = open('../data/TrainingData/scenario_test1x10.json')
-data = json.load(f)
-tripAttr = data["demand"]
-reb_time = data["rebTime"]
-total_acc = data["totalAcc"]
-
-scenario = Scenario(charging_stations=chargers, number_charge_levels=number_charge_levels, sd=10, tripAttr = tripAttr, demand_ratio=1, reb_time=reb_time, total_acc = total_acc)
-env = AMoD(scenario, beta=0.5)
-
 # set Gurobi environment
 gurobi_env = gp.Env(empty=True)
 gurobi_env.setParam('WLSACCESSID', '8cad5801-28d8-4e2e-909e-3a7144c12eb5')
@@ -35,7 +19,11 @@ gurobi_env.setParam('LICENSEID', 799876)
 gurobi_env.setParam("OutputFlag",0)
 gurobi_env.start()
 
-mpc = MPC(env, gurobi_env)
+# MPC exact
+scenario = Scenario(json_file="data/scenario_nyc4x4.json", sd=10, demand_ratio=.5, json_hr=7,json_tstep=3)
+env = AMoD(scenario, beta=0.5)
+
+mpc = MPC(env, CPLEXPATH,T=10)
 opt_rew = []
 obs = env.reset()
 done = False
@@ -50,6 +38,7 @@ while(not done):
     paxAction, rebAction = mpc.MPC_exact() 
     time_i_end = time.time()
     t_i = time_i_end - time_i_start
+#     print(f"t_i: {t_i:.1f}sec")
     time_list.append(t_i)
     
     obs, reward1, done, info = env.pax_step(paxAction)
@@ -61,5 +50,54 @@ while(not done):
     opcost += info['operating_cost']
     revenue += info['revenue'] 
 print(f'MPC: Reward {sum(opt_rew)}, Revenue {revenue},Served demand {served}, Rebalancing Cost {rebcost}, Operational Cost {opcost}, Avg.Time: {np.array(time_list).mean():.2f} +- {np.array(time_list).std():.2f}sec')
+demand = sum([env.demand[i,j][t] for i,j in env.demand for t in range(0,60)])
+print(demand, served/demand)
+
+# three levels
+# scenario = Star2Complete(sd = 10, grid_travel_time = 2, T = 16, star_demand = 7.5, complete_demand=1.5, 
+#                         star_center = [9,10,13,14], beta=0.9, alpha = 0.5, ninit = 200, fix_price = True)
+
+scenario = Scenario(json_file="data/scenario_nyc4x4.json", sd=10, demand_ratio=.5, json_hr=7,json_tstep=3)
+env = AMoD(scenario, beta=0.5)
+
+mpc = MPC(env, CPLEXPATH,T=10)
+opt_rew = []
+obs = env.reset()
+done = False
+served = 0
+rebcost = 0
+opcost = 0
+revenue = 0
+paxFlow = dict()
+rebFlow = dict()
+desiredAcc = dict()
+rebAction = dict()
+t_0 = time.time()
+time_list = []
+action_list = []
+for _ in range(100):
+    try:
+        while(not done):
+            t = env.time
+            time_i_start = time.time()
+            res_path = 'tri-level/'
+            desiredAcc[t], paxFlow[t], rebFlow[t] = mpc.tri_level()
+            action_list.append(list(desiredAcc[t].values()))
+            time_i_end = time.time()
+            t_i = time_i_end - time_i_start
+            time_list.append(t_i)
+            print(f"t_i: {t_i:.1f}sec")
+            obs, reward1, done, info = env.pax_step(CPLEXPATH = CPLEXPATH, PATH = res_path)
+
+            rebAction[t] = solveRebFlow(env,'reb_'+res_path,desiredAcc[t],CPLEXPATH)
+            obs, reward2, done, info = env.reb_step(rebAction[t])
+            opt_rew.append(reward1+reward2) 
+            served += info['served_demand']
+            rebcost += info['rebalancing_cost']
+            opcost += info['operating_cost']
+            revenue += info['revenue']
+    except:
+        continue
+print(f'Downgraded MPC (three levels): Reward {sum(opt_rew)}, Revenue {revenue},Served demand {served}, Rebalancing Cost {rebcost}, Operational Cost {opcost}, Avg.Time: {np.array(time_list).mean():.1f} +- {np.array(time_list).std():.1f}sec')
 demand = sum([env.demand[i,j][t] for i,j in env.demand for t in range(0,60)])
 print(demand, served/demand)
