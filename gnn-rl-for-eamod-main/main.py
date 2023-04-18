96bc749514f831b5a4f0b42a946521b072fb9127
AaryanSinghal <aaryan0404@gmail.com>
2023-04-17 23:14:20 -0700

    m

--- gnn-rl-for-eamod-main/main.py
+++ gnn-rl-for-eamod-main/main.py
@@ -264,7 +264,6 @@ for i_episode in epochs:
         # solve minimum rebalancing distance problem (Step 3 in paper)
         if step == 0 and i_episode == 0:
             # initialize optimization problem in the first step
-            print(desired_acc)
             rebal_flow_solver = RebalFlowSolver(env=env, desiredAcc=desired_acc, gurobi_env=gurobi_env)
         else:
             rebal_flow_solver.update_constraints(desired_acc, env)
