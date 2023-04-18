07cc2c5160b50881246594e688de2e72eef4bcc5
AaryanSinghal <aaryan0404@gmail.com>
2023-04-18 00:06:08 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/reb_flows_solver.py
+++ gnn-rl-for-eamod-main/src/algos/reb_flows_solver.py
@@ -1,63 +1,77 @@
 # Class def for optimization
 import gurobipy as gp
-from gurobipy import quicksum
 
 
-class RebalFlowSolver:  
-    def __init__(self, env, desiredAcc, gurobi_env):
+class RebalFlowSolver:
+    def __init__(self, env, desired_acc_spatial, gurobi_env=None):
         # Initialize model
-        self.cons_charge_graph1 = {}
-        self.cons_charge_graph2 = {}
-        self.cons_spatial_graph_charging_cars = {}
+        self.cons_charge_graph = {}
+        self.cons_spatial_graph = {}
         t = env.time
+        # start_time = os.times()
         self.m = gp.Model(env=gurobi_env)
-        self.flow = self.m.addMVar(shape=(len(env.edges)), lb=0, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="flow") # both could be INTEGER
-        self.slack_variables = self.m.addMVar(shape=(len(env.nodes)), lb=-1000000000, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="slack")
+        self.flow = self.m.addMVar(shape=(len(env.edges)), lb=0.0, ub=gp.GRB.INFINITY,
+                                   vtype=gp.GRB.CONTINUOUS, name="flow")  # both could be INTEGER
+        
+        acc_spatial_with_charge = dict()
+        for n in env.nodes_spatial:
+            acc_spatial_with_charge[n] = env.acc_spatial[n][env.time + 1]
+            for c in range(env.scenario.max_energy_distance):
+                acc_spatial_with_charge[n] -= env.acc[(n,c)][env.time + 1]
+        
+        # should be unnecessary:
+        for e_idx in range(len(env.edges)):
+            self.m.addConstr(self.flow[e_idx] >= 0.)
+        for e_charging in env.charging_edges:
+            self.m.addConstr(self.flow[e_charging] == 0.)
 
+        # Constraint 0: We can not have more vehicles flowing out of a node, than vehicles at the node
         for n_idx in range(len(env.nodes)):
             n = env.nodes[n_idx]
             outgoing_edges = env.map_node_to_outgoing_edges[n]
-            incoming_edges = env.map_node_to_incoming_edges[n]
+            self.cons_charge_graph[n_idx] = self.m.addConstr(sum(self.flow[outgoing_edges]) <= env.acc[n][t + 1])
 
-            # Constraint 1: We can not have more vehicles flowing out of a node, than vehicles at the node
-            self.cons_charge_graph1[n_idx] = self.m.addConstr(sum(self.flow[outgoing_edges]) <= env.acc[n][t + 1])
-
-            # Constraint 2: We want to reach the target distribrution
-            self.cons_charge_graph2[n_idx] = self.m.addConstr(sum(self.flow[incoming_edges]) - sum(self.flow[outgoing_edges]) + self.slack_variables[n_idx] == desiredAcc[n] - env.acc[n][t + 1]) 
-            
-            # Constraint 3: We cannot charge more vehicles then we have charging spots
-        for r_idx in range(env.number_nodes_spatial):
-            outgoing_charge_edges = env.map_region_to_charge_edges[r_idx]
-            self.cons_spatial_graph_charging_cars[r_idx] = self.m.addConstr(sum(self.flow[outgoing_charge_edges]) <= env.scenario.cars_per_station_capacity[r_idx] - env.scenario.cars_charging_per_station[r_idx][t+1]) # TODO finish
-            # self.cons_spatial_graph_charging_cars[r_idx] = self.m.addConstr(env.n_charging_vehicles_spatial[r_idx][t+1] <= env.scenario.cars_per_station_capacity[r_idx])
+        # Constraint 1: should only use road edges to rebalance idle vehicles to match target distribution.
+        for node_spatial in env.nodes_spatial:
+            corresponing_road_edges_charge_graph_incoming = env.map_node_spatial_to_incoming_road_edges_charge_graph[
+                node_spatial]
+            corresponing_road_edges_charge_graph_outgoing = env.map_node_spatial_to_outgoing_road_edges_charge_graph[
+                node_spatial]
+            self.cons_spatial_graph[node_spatial] = self.m.addConstr(sum(self.flow[corresponing_road_edges_charge_graph_incoming]) - sum(self.flow[corresponing_road_edges_charge_graph_outgoing]) ==
+                desired_acc_spatial[node_spatial] - acc_spatial_with_charge[node_spatial]) 
         
-        self.obj1 = 0
-        self.obj2 = 0
-        for n_idx in range(len(env.nodes)):
-            self.obj1 += self.slack_variables[n_idx] * 1e10
-
+        self.obj = 0
         for e_idx in range(len(env.edges)):
-            i,j = env.edges[e_idx]
-            self.obj2 += self.flow[e_idx] * (env.G.edges[i,j]['time'][t + 1]+env.scenario.time_normalizer) * env.scenario.operational_cost_per_timestep
-        self.m.setObjective(self.obj1+self.obj2, gp.GRB.MINIMIZE)
+            i, j = env.edges[e_idx]
+            self.obj += self.flow[e_idx] * (env.G.edges[i, j]['time']
+                                             [t + 1] + env.scenario.time_normalizer)* env.scenario.operational_cost_per_timestep
+        self.m.setObjective(self.obj, gp.GRB.MINIMIZE)
+        # end_time = os.times()
+        # print("Time for creating model: ", end_time[0] - start_time[0])
+        self.env = env
 
-    def update_constraints(self, desired_acc, env):
-        desired_acc_checksum = 0
-        acc_checksum = 0
+    def update_constraints(self, desired_acc_spatial, env):
+        acc_spatial_with_charge = dict()
+        for n in env.nodes_spatial:
+            acc_spatial_with_charge[n] = env.acc_spatial[n][env.time + 1]
+            for c in range(env.scenario.max_energy_distance):
+                acc_spatial_with_charge[n] -= env.acc[(n,c)][env.time + 1]
+        test_sum_desired = 0
+        test_sum_acc = 0
+        for node_spatial in env.nodes_spatial:
+            test_sum_desired += desired_acc_spatial[node_spatial]
+            test_sum_acc += acc_spatial_with_charge[node_spatial]
+            self.cons_spatial_graph[node_spatial].RHS = desired_acc_spatial[node_spatial] - acc_spatial_with_charge[node_spatial]
+        assert abs(test_sum_desired - test_sum_acc) < 0.00001
         for n_idx in range(len(env.nodes)):
             node_charge = env.nodes[n_idx]
-            desired_acc_checksum += desired_acc[node_charge]
-            acc_checksum += env.acc[node_charge][env.time + 1]
-            self.cons_charge_graph1[n_idx].RHS = env.acc[node_charge][env.time + 1]
-            self.cons_charge_graph2[n_idx].RHS = desired_acc[node_charge] - env.acc[node_charge][env.time + 1]
-        assert abs(desired_acc_checksum - acc_checksum) < 1e-5
-        for r_idx in range(env.number_nodes_spatial):
-            self.cons_spatial_graph_charging_cars[r_idx].RHS = env.scenario.cars_per_station_capacity[r_idx] - env.scenario.cars_charging_per_station[r_idx][env.time+1]
+            self.cons_charge_graph[n_idx].RHS = env.acc[node_charge][env.time + 1]
         self.m.update()
-        
+
     def update_objective(self, env):
-        self.obj2 = sum((self.flow[e_idx] * (env.G.edges[env.edges[e_idx][0], env.edges[e_idx][1]]['time'][env.time + 1] + env.scenario.time_normalizer) * env.scenario.operational_cost_per_timestep) for e_idx in range(len(env.edges)))
-        self.m.setObjective(self.obj1+self.obj2, gp.GRB.MINIMIZE)
+        self.obj = sum((self.flow[e_idx] * (env.G.edges[env.edges[e_idx][0], env.edges[e_idx][1]]
+                        ['time'][env.time + 1] + env.scenario.time_normalizer) * env.scenario.operational_cost_per_timestep) for e_idx in range(len(env.edges)))
+        self.m.setObjective(self.obj, gp.GRB.MINIMIZE)
         self.m.update()
 
     def optimize(self):
@@ -67,4 +81,3 @@ class RebalFlowSolver:
         assert self.m.status == 2
         action = self.flow.X
         return action
-        
