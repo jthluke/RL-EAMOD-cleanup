05e2d49051ea44e0c50891e83faf47e030bff21a
AaryanSinghal <aaryan0404@gmail.com>
2023-05-10 15:33:17 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -77,7 +77,7 @@ class GNNParser():
             new_edge = torch.tensor([[origin_node_idx], [destination_node_idx]], dtype=torch.long)
             edge_idx = torch.cat((edge_idx, new_edge), 1)
         edge_index = edge_idx
-        print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 12
+        # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 12
 
         # V2 - combination of V0 and V1
         # (A)
