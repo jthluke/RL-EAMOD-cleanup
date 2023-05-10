4d08d064c8650ba525ef95e8aeec1d26a259ff62
AaryanSinghal <aaryan0404@gmail.com>
2023-05-10 14:48:39 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -88,19 +88,35 @@ class GNNParser():
         # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) = 12
 
         # V3 - grid style one-hop connections
+        edges = []
+        for o in self.env.nodes:
+            for d in self.env.nodes:
+                if ((o[1] == d[1]) or ((o[1] == d[1] - 1) and (o[0] == d[0]))):
+                    edges.append([o, d])
+        
+        edge_idx = torch.tensor([[], []], dtype=torch.long)
+        for e in edges:
+            origin_node_idx = self.env.nodes.index(e[0])
+            destination_node_idx = self.env.nodes.index(e[1])
+            new_edge = torch.tensor([[origin_node_idx], [destination_node_idx]], dtype=torch.long)
+            edge_idx = torch.cat((edge_idx, new_edge), 1)
+        edge_index = edge_idx
+        print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1]))
+        
+        # V4 - combination of V1 and V2
         # edges = []
         # for o in self.env.nodes:
         #     for d in self.env.nodes:
-        #         if ((o[1] == d[1]) or ((o[1] == d[1] - 1) and (o[0] == d[0]))):
+        #         if (o[0] == d[0] and o[1] == d[1]):
         #             edges.append([o, d])
-        
         # edge_idx = torch.tensor([[], []], dtype=torch.long)
         # for e in edges:
         #     origin_node_idx = self.env.nodes.index(e[0])
         #     destination_node_idx = self.env.nodes.index(e[1])
         #     new_edge = torch.tensor([[origin_node_idx], [destination_node_idx]], dtype=torch.long)
         #     edge_idx = torch.cat((edge_idx, new_edge), 1)
-        # edge_index = edge_idx
+        # edge_index = torch.cat((edge_idx, self.env.gcn_edge_idx), 1)
+        # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) = 32
 
         # default/global return
         data = Data(x, edge_index)
