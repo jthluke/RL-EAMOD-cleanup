5d22cc40d562119eb2f601c4ce61b94a09054e85
AaryanSinghal <aaryan0404@gmail.com>
2023-05-13 02:10:28 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -111,10 +111,27 @@ class GNNParser():
         # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 32
         
         # V4 - combination of V3 and V1
+        # edges = []
+        # for o in self.env.nodes:
+        #     for d in self.env.nodes:
+        #         if ((o[1] == d[1] and o[0] == d[0]) or (o[1] == d[1] and o[0] != d[0]) or ((o[1] == d[1] - 1) and (o[0] == d[0])) or ((o[1] == d[1] + 1) and (o[0] == d[0]))):
+        #             edges.append([o, d])
+        
+        # edge_idx = torch.tensor([[], []], dtype=torch.long)
+        # for e in edges:
+        #     origin_node_idx = self.env.nodes.index(e[0])
+        #     destination_node_idx = self.env.nodes.index(e[1])
+        #     new_edge = torch.tensor([[origin_node_idx], [destination_node_idx]], dtype=torch.long)
+        #     edge_idx = torch.cat((edge_idx, new_edge), 1)
+        # edge_index = edge_idx
+        # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 44
+
+        # V5 - all edges + artificial edges
+        charge_delta = 3
         edges = []
         for o in self.env.nodes:
             for d in self.env.nodes:
-                if ((o[1] == d[1] and o[0] == d[0]) or (o[1] == d[1] and o[0] != d[0]) or ((o[1] == d[1] - 1) and (o[0] == d[0])) or ((o[1] == d[1] + 1) and (o[0] == d[0]))):
+                if ((o[0] != d[0]) and (o[1] + charge_delta == d[1])):
                     edges.append([o, d])
         
         edge_idx = torch.tensor([[], []], dtype=torch.long)
@@ -123,8 +140,9 @@ class GNNParser():
             destination_node_idx = self.env.nodes.index(e[1])
             new_edge = torch.tensor([[origin_node_idx], [destination_node_idx]], dtype=torch.long)
             edge_idx = torch.cat((edge_idx, new_edge), 1)
+        edge_idx = torch.cat((edge_idx, self.env.gcn_edge_idx), 1)
         edge_index = edge_idx
-        # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 44
+        print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 24
 
         # Add evaluation mode to code base with greedy mean parameter extarction from dirchilet 
         # Finish V0 - V5 (with artificial edges added)
