a7f19c52074bd279a80e94b4ac5da14334ef2f99
AaryanSinghal <aaryan0404@gmail.com>
2023-05-17 14:58:22 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -153,7 +153,7 @@ class GNNParser():
             edge_idx = torch.cat((edge_idx, new_edge), 1)
         edge_idx = torch.cat((edge_idx, self.env.gcn_edge_idx), 1)
         edge_index = edge_idx
-        print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 48
+        # print("# of EDGES PASSED TO GCN" + str(edge_index.shape[1])) # = 48
 
         # V6 - all edges + artificial edges + "infeasible" charge edges + "unintuitive" road edges
         # charge_delta = 4
