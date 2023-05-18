9e1334fe58f1cd3847874e5700859139a50fb529
AaryanSinghal <aaryan0404@gmail.com>
2023-05-18 16:40:49 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -197,7 +197,7 @@ class GNNParser():
             all_times.extend(times_for_e)
         # Convert the list of 'time' values into a tensor.
         tensor = torch.tensor(all_times)
-        e = (tensor.view(1, np.prod(tensor.shape)).float()).squeeze(0).view(self.input_size, len(edges)).T
+        e = (tensor.view(1, np.prod(tensor.shape)).float()).squeeze(0).view(self.input_size, (self.input_size)/(np.prod(tensor.shape))).T
         data = Data(x, edge_index, edge_attr=e)
         
 
