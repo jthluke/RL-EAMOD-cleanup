9cec723229a94b8d73411cd534c183280e9c4607
AaryanSinghal <aaryan0404@gmail.com>
2023-05-18 19:30:24 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -286,9 +286,9 @@ class GNNActor(nn.Module):
         self.hidden_dim = hidden_dim
         
         self.conv1 = EdgeConv(node_size, edge_size, hidden_dim)
-        self.h_to_mu = nn.Linear((22 * 3) + hidden_dim, out_channels)
-        self.h_to_sigma = nn.Linear((22 * 3) + hidden_dim, out_channels)
-        self.h_to_concentration = nn.Linear((22 * 3) + hidden_dim, out_channels)
+        self.h_to_mu = nn.Linear(node_size + 2 + hidden_dim, out_channels)
+        self.h_to_sigma = nn.Linear(node_size + 2 + hidden_dim, out_channels)
+        self.h_to_concentration = nn.Linear(node_size + 2 + hidden_dim, out_channels)
 
     def forward(self, x, edge_index, edge_attr):
         x_pp = self.conv1(x, edge_index, edge_attr)
