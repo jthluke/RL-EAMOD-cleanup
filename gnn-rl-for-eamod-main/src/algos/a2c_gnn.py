106450403bd378f7e2cc26ed76f85026e386cbc3
AaryanSinghal <aaryan0404@gmail.com>
2023-06-14 02:39:36 -0700

    m

--- gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main/src/algos/a2c_gnn.py
@@ -246,11 +246,11 @@ class EdgeConv(MessagePassing):
         # x_i has shape [E, in_channels]
         # x_j has shape [E, in_channels]
 
-        print("x_i shape: " + str(x_i.shape))
-        print("x_j shape: " + str(x_j.shape))
-        print("edge_attr shape: " + str(edge_attr.shape))
+        # print("x_i shape: " + str(x_i.shape))
+        # print("x_j shape: " + str(x_j.shape))
+        # print("edge_attr shape: " + str(edge_attr.shape))
         tmp = torch.cat([x_i, x_j, edge_attr], dim=1)  # tmp has shape [E, 2 * in_channels]
-        # print("tmp shape: " + str(tmp.shape))
+        print("tmp shape: " + str(tmp.shape))
         return self.mlp(tmp)
 
 #########################################
@@ -292,7 +292,7 @@ class GNNActor(nn.Module):
         self.hidden_dim = hidden_dim
         
         self.conv1 = EdgeConv(node_size, edge_size, hidden_dim)
-        self.conv2 = EdgeConv(node_size, edge_size, hidden_dim) # second edge convolution layer
+        self.conv2 = EdgeConv(hidden_dim, edge_size, hidden_dim) # second edge convolution layer
 
         self.lin1 = nn.Linear(22 + hidden_dim, hidden_dim)
         self.lin2 = nn.Linear(hidden_dim, hidden_dim) # second linear layer
