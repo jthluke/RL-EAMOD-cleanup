615cdc35a86b40e85b2552419f027043de9c6aba
AaryanSinghal <aaryan0404@gmail.com>
2023-08-11 11:59:52 -0700

    m

--- gnn-rl-for-eamod-main-EdgeParsings/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main-EdgeParsings/src/algos/a2c_gnn.py
@@ -210,7 +210,7 @@ class GNNParser():
         
             edge_attr = []
             # edges.extend(self.env.edges) # needed when adding self-loops only
-            print(edge_index)
+            print(np.shape(edge_index))
             for edg_idx in edge_index:
                 e = [self.env.nodes[edg_idx[0]], self.env.nodes[edg_idx[1]]]
 
