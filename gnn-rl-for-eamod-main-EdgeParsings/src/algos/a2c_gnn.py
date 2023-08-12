4ee16cf2b69b7524f6bab8f341c1dded8724b501
AaryanSinghal <aaryan0404@gmail.com>
2023-08-12 16:35:51 -0700

    m

--- gnn-rl-for-eamod-main-EdgeParsings/src/algos/a2c_gnn.py
+++ gnn-rl-for-eamod-main-EdgeParsings/src/algos/a2c_gnn.py
@@ -263,7 +263,7 @@ class GNNParser():
                           for t in range(self.env.time+1, self.env.time+self.T+1)]).view(1, self.T, self.env.number_nodes_spatial).float(),
             torch.tensor([[sum([self.env.price[o,j][t]*self.scale_factor*self.price_scale_factor*(self.env.demand[o,j][t]) \
                           for j in self.env.region]) for o in self.env.region] for t in range(self.env.time+1, self.env.time+self.T+1)]).view(1, self.T, self.env.number_nodes_spatial).float()),
-              dim=1).squeeze(0).view(self.input_size, self.env.number_nodes_spatial).T
+              dim=1).squeeze(0).view(1 + self.T + self.T, self.env.number_nodes_spatial).T
         edge_index  = self.env.gcn_edge_idx_spatial
 
         edge_attr = []
