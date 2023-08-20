29e53c206b31acfa4db62f2a91f4cac24e74a41b
AaryanSinghal <aaryan0404@gmail.com>
2023-08-20 15:59:22 -0700

    m

--- gnn-rl-for-eamod-main-heuristic/src/envs/amod_env.py
+++ gnn-rl-for-eamod-main-heuristic/src/envs/amod_env.py
@@ -421,7 +421,7 @@ class AMoD:
     def reb_step(self, rebAction):
         t = self.time
         self.reward = 0  # reward is calculated from before this to the next rebalancing, we may also have two rewards, one for pax matching and one for rebalancing
-
+        rebreward_internal = 0
         self.rebAction = rebAction
         # rebalancing
         for k in range(len(self.edges)):
@@ -505,13 +505,21 @@ class AMoD:
             for region2 in self.nodes_spatial:
                 demand += self.demand[region, region2][self.time+1]
             unserved_demand = demand - self.acc_spatial[region][self.time+1]
-            wasted_customers_penalty = min(0, unserved_demand * (-10))
+            wasted_customers_penalty += min(0, unserved_demand * (-10))
+        
+        overconcentration_penalty = 0
+        total_vehicles = sum(self.acc[(region, charge_level)][self.time+1] for region in self.nodes_spatial for charge_level in range(self.scenario.number_charge_levels))
+
+        for region in self.nodes_spatial:
+            total_vehicles_region = sum(self.acc[(region, charge_level)][self.time+1] for charge_level in range(self.scenario.number_charge_levels))
+            if total_vehicles_region >= 0.8 * total_vehicles:
+                overconcentration_penalty -= 1000
 
         # print("reward: " + str(self.reward))
         # print("charging_penalty: " + str(charging_penalty))
         # print("wasted_customers_penalty: " + str(wasted_customers_penalty))
         
-        rebreward_internal = self.reward + wasted_customers_penalty
+        rebreward_internal = self.reward + wasted_customers_penalty + overconcentration_penalty
 
         self.time += 1
         # use self.time to index the next time step
