83c1974a9401572dce4a2f64efc8af01995538f4
AaryanSinghal <aaryan0404@gmail.com>
2023-09-02 22:18:26 -0700

    m

--- gnn-rl-for-eamod-main-CQL/main.py
+++ gnn-rl-for-eamod-main-CQL/main.py
@@ -51,12 +51,9 @@ class ReplayData:
         self.rew_scale = rew_scale
 
     def create_dataset(self, edge_index, memory_path, size=60000, st=False, sc=False):
-        with open(f'data/NY/ClusterDataset1/{memory_path}.pkl', 'rb') as f:
-            header = struct.unpack('>I', f.read(4))
-            packet_type = header[0]
-            packet_length = header[1]
-            packet_data = f.read(packet_length)
-        print(packet_data)
+        with open('file.pkl', 'rb') as f:
+            object = pickle.load(f)
+        print(object)
         if st:
             mean = data['rew'].mean()
             std = data['rew'].std()
