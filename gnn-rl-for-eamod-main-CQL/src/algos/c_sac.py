460a3eacb282b4415a07dfd409676100f1234ea6
AaryanSinghal <aaryan0404@gmail.com>
2023-09-03 00:15:35 -0700

    m

--- gnn-rl-for-eamod-main-CQL/src/algos/c_sac.py
+++ gnn-rl-for-eamod-main-CQL/src/algos/c_sac.py
@@ -272,10 +272,10 @@ class SAC(nn.Module):
          reward_batch,
          action_batch) = torch.from_numpy(np.array(data.x_s)), data.edge_index_s, torch.from_numpy(np.array(data.x_t)), data.edge_index_t, data.reward, torch.from_numpy(np.array(data.action).reshape(-1, self.env.number_nodes))
         
-        print(state_batch.shape)
-        print(type(state_batch))
-        print(edge_index.shape)
-        print(type(edge_index))
+        # print(state_batch.shape)
+        # print(type(state_batch))
+        # print(edge_index.shape)
+        # print(type(edge_index))
         
         q1 = self.critic1(state_batch, edge_index, action_batch)
         q2 = self.critic2(state_batch, edge_index, action_batch)
