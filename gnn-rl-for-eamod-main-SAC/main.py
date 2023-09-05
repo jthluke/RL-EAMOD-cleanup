baec47b75417db66ff05868b67f0125d3b705a90
AaryanSinghal <aaryan0404@gmail.com>
2023-09-05 00:25:08 -0700

    m

--- gnn-rl-for-eamod-main-SAC/main.py
+++ gnn-rl-for-eamod-main-SAC/main.py
@@ -212,13 +212,13 @@ if not args.test:
             a = data[key][1]
             r = data[key][2] * args.rew_scale
             o_2 = data[key][3]
-            print(o_1.dtype)
+            print(o_1)
             print(o_1.shape)
-            print(a.dtype)
+            print(a)
             print(a.shape)
-            print(r.dtype)
+            print(r)
             print(r.shape)
-            print(o_2.dtype)
+            print(o_2)
             print(o_2.shape)
             model.replay_buffer.store(o_1, a, r, o_2)
 
@@ -263,13 +263,13 @@ if not args.test:
             episode_reward += paxreward
             if step > 0:
                 rl_reward = (paxreward + rebreward)
-                print(obs1.dtype)
+                print(obs1)
                 print(obs1.shape)
-                print(action_rl.dtype)
+                print(action_rl)
                 print(action_rl.shape)
-                print(rl_reward.dtype)
+                print(rl_reward)
                 print(rl_reward.shape)
-                print(o.dtype)
+                print(o)
                 print(o.shape)
                 model.replay_buffer.store(
                     obs1, action_rl, args.rew_scale * rl_reward, o)
