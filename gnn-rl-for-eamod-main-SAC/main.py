3e561a2f7c2b11b7faf23e000693c352b7236324
AaryanSinghal <aaryan0404@gmail.com>
2023-09-04 23:41:23 -0700

    m

--- gnn-rl-for-eamod-main-SAC/main.py
+++ gnn-rl-for-eamod-main-SAC/main.py
@@ -203,6 +203,18 @@ if not args.test:
         critic_version=args.critic_version,
     ).to(device)
 
+    # get .pkl file from data folder
+    with open(os.path.join('data', problem_folder, 'ClusterDataset1', f'MPC_SARS_{checkpoint_path}.pkl'), 'rb') as f:
+        data = pickle.load(f)
+        
+        for key in data.keys():
+            o_1 = data[key][0]
+            a = data[key][1]
+            r = data[key][2] * args.rew_scale
+            o_2 = data[key][3]
+            model.replay_buffer.store(o_1, a, r, o_2)
+
+
     train_episodes = args.max_episodes  # set max number of training episodes
     epochs = trange(train_episodes)  # epoch iterator
     best_reward = -np.inf  # set best reward
