353993b2628aeaff1c090359050e61981df197d1
AaryanSinghal <aaryan0404@gmail.com>
2023-09-06 19:17:11 -0700

    m

--- gnn-rl-for-eamod-main-heuristic/mpc_baselines/main_mpc.py
+++ gnn-rl-for-eamod-main-heuristic/mpc_baselines/main_mpc.py
@@ -69,10 +69,13 @@ if args.toy:
 else:
     # problem_folder = 'NY_5'
     # file_path = os.path.join('..', 'data', problem_folder, 'NY_5.json')
-    problem_folder = 'NY/ClusterDataset1'
-    file_path = os.path.join('..', 'data', problem_folder,  'd1.json')
+    # problem_folder = 'NY/ClusterDataset1'
+    # file_path = os.path.join('..', 'data', problem_folder,  'd1.json')
     # problem_folder = 'SF_5_clustered'
     # file_path = os.path.join('..', 'data', problem_folder, 'SF_5_short_afternoon_test.json')
+    problem_folder = 'SF_10_clustered'
+    file_path = os.path.join('..', 'data', problem_folder,  'SF_10.json')
+    
     experiment = problem_folder +  '_mpc_horizon_' + str(mpc_horizon) + 'entire_problem' + file_path + "_heuristic_graph"
     energy_dist_path = os.path.join('..', 'data', problem_folder, 'energy_distance.npy')
     test_scenario = create_scenario(file_path, energy_dist_path)
