0603220d9c1d971fe570fc1f418c2dbcb9eb6e3e
AaryanSinghal <aaryan0404@gmail.com>
2023-10-02 09:42:09 -0700

    m

--- gnn-rl-for-eamod-main-SAC/src/algos/reb_flows_solver.py
+++ gnn-rl-for-eamod-main-SAC/src/algos/reb_flows_solver.py
@@ -16,7 +16,7 @@ class RebalFlowSolver:
         self.m.Params.Method = 2
         self.m.Params.Crossover = 0
         self.m.Params.BarConvTol = 1e-6
-        self.m.Params.Threads = 64
+        self.m.Params.Threads = 60
 
         self.flow = self.m.addMVar(shape=(len(env.edges)), lb=0, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="flow") # both could be INTEGER
         self.slack_variables = self.m.addMVar(shape=(len(env.nodes)), lb=-10000000, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="slack")
