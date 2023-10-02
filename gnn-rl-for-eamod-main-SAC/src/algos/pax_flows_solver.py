0ed40da1abbaf73491a29bb4562bd08dd6bfa9cd
AaryanSinghal <aaryan0404@gmail.com>
2023-10-01 22:59:50 -0700

    m

--- gnn-rl-for-eamod-main-SAC/src/algos/pax_flows_solver.py
+++ gnn-rl-for-eamod-main-SAC/src/algos/pax_flows_solver.py
@@ -17,7 +17,7 @@ class PaxFlowsSolver:
         self.m.Params.Method = 2
         self.m.Params.Crossover = 0
         self.m.Params.BarConvTol = 1e-6
-        self.m.Params.Threads = 120
+        self.m.Params.Threads = 64
 
         self.flow = self.m.addMVar(shape=(len(
             self.env.edges)), lb=0.0, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="flow")
