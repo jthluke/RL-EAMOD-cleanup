8dc9f7d1e3848fbc3aebd3160f71a322af1b7615
AaryanSinghal <aaryan0404@gmail.com>
2023-10-03 11:50:58 -0700

    m

--- gnn-rl-for-eamod-main-SAC/src/algos/pax_flows_solver.py
+++ gnn-rl-for-eamod-main-SAC/src/algos/pax_flows_solver.py
@@ -77,7 +77,7 @@ class PaxFlowsSolver:
         self.m.update()
         time_c_end = time.time() - time_c
 
-        print(f"Time a: {time_a_end}, Time b: {time_b_end}, Time c: {time_c_end}")
+        print(f"Time: {time_a_end}, {time_b_end}, {time_c_end}")
 
     # def optimize(self):
     #     self.m.optimize()
