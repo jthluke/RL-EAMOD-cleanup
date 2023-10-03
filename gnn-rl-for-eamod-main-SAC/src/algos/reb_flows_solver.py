c58b0a84942bfc1520a749505f7dfc5ebe2f2c56
AaryanSinghal <aaryan0404@gmail.com>
2023-10-03 13:44:25 -0700

    m

--- gnn-rl-for-eamod-main-SAC/src/algos/reb_flows_solver.py
+++ gnn-rl-for-eamod-main-SAC/src/algos/reb_flows_solver.py
@@ -22,7 +22,7 @@ class RebalFlowSolver:
         self.m.Params.Method = 2
         self.m.Params.Crossover = 0
         self.m.Params.BarConvTol = 1e-6
-        self.m.Params.Threads = 1024
+        self.m.Params.Threads = 5000
         self.m.setParam("LogFile", os.path.join(os.getcwd(), 'reb_flow_gurobi_log.log'))
 
         self.flow = self.m.addMVar(shape=(len(env.edges)), lb=0, ub=gp.GRB.INFINITY, vtype=gp.GRB.CONTINUOUS, name="flow") # both could be INTEGER
