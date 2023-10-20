c8af77260988a876645351d945a44274ffe0b010
AaryanSinghal <aaryan0404@gmail.com>
2023-10-19 19:44:11 -0700

    m

--- gnn-rl-for-eamod-main/mpc_baselines/src/envs/amod_env.py
+++ gnn-rl-for-eamod-main/mpc_baselines/src/envs/amod_env.py
@@ -65,7 +65,8 @@ class AMoD:
             self.noisy_demand = defaultdict(dict)
             for (o, d), demand_data in self.scenario.demand_input.items():
                 for t, v in demand_data.items():
-                    self.noisy_demand[(o, d)][t] = np.random.normal(v, v * 0.1 * t, 1)[0]
+                    noise = 0.01 * t * v  # Linear growing noise
+                    self.noisy_demand[(o, d)][t] = v + noise
 
             self.price = self.scenario.p
             # number of vehicles within each node, key: i - node, t - time
